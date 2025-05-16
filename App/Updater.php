79ab23c8cccbb520a57f8a3ece632a4b563ed822
maxkonar <k.karasiewicz@premiumdigital.pl>
2025-05-16 10:44:13 +0200

    add env

--- App/Updater.php
+++ App/Updater.php
@@ -3,14 +3,33 @@
 namespace PdExtraWidgets;
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+use Dotenv\Dotenv;
 
 class Updater
 {
     public static function init()
     {
-        $pluginFile = PD_EXTRA_WIDGETS_PLUGIN_DIR_PATH . '/pd-extra-widgets.php';
+        if (!file_exists(PD_EXTRA_WIDGETS_PLUGIN_DIR_PATH . '.env')) {
+            error_log('[PD Extra Widgets] .env file not found. Please create a .env file with the GITHUB_REPO variable.');
+            return;
+        }
 
-        $repoUrl = 'https://github.com/maxkonar/pd-extra-widgets';
+        if (!class_exists(PucFactory::class)) {
+            error_log('[PD Extra Widgets] Plugin Update Checker is not available.');
+            return;
+        }
+
+        $dotenv = Dotenv::createImmutable(PD_EXTRA_WIDGETS_PLUGIN_DIR_PATH);
+        $dotenv->load();
+
+        $repoUrl = $_ENV['GITHUB_REPO'] ?? '';
+
+        if (empty($repoUrl)) {
+            error_log('[PD Extra Widgets] GITHUB_REPO is not set in your .env file.');
+            return;
+        }
+
+        $pluginFile = PD_EXTRA_WIDGETS_PLUGIN_DIR_PATH . '/pd-extra-widgets.php';
 
         $updateChecker = PucFactory::buildUpdateChecker(
             $repoUrl,
@@ -19,6 +38,5 @@ class Updater
         );
 
         $updateChecker->getVcsApi()->enableReleaseAssets();
-
     }
 }
